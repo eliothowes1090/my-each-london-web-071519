e2d9aa43700d17bf7a394291646503d3b185598b
Eliot Howes <ejhowes@gmail.com>
2019-05-26 15:32:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
+def my_each(words) # put argument(s) here
   # code here
+  counter = 0
+  while counter < words.length do |word|
+    yield(word)
+    counter += 1
+  end
 end
