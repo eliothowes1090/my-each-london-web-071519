2307431e56796951d796554e9006a4b753d57efb
Eliot Howes <ejhowes@gmail.com>
2019-05-26 15:33:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(words) # put argument(s) here
   # code here
   counter = 0
-  while counter < words.length do |word|
-    yield(word)
+  while counter < words.length
+    yield(words)
     counter += 1
   end
 end
